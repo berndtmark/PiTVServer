92dbd3f1173cda1dfbbb0d73877eced67764d1e4
Mark Berndt <berndtmark@gmail.com>
2019-09-07 16:47:26 +0100

    init file corrected

--- init.sh
+++ init.sh
@@ -8,10 +8,5 @@ echo "execute..."
 $SCRIPTS/global-packages.sh
 $SCRIPTS/sabnzb.sh
 $SCRIPTS/plex.sh
-$SCRIPTS/sickbeard.sh
-
-echo "pm2 to autostart..."
-# pm2 startup
-## this will change - use `pm2 startup` to get script
-sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u pi --hp /home/pi
-pm2 save
+#$SCRIPTS/sickbeard.sh
+$SCRIPTS/sonarr.sh

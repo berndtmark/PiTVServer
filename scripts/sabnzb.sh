1ed9622b5243d20b66571d7f5b20a09fb231e47f
Mark Berndt <berndtmark@gmail.com>
2019-10-05 17:40:02 +0100

    script made re-runnable

--- scripts/sabnzb.sh
+++ scripts/sabnzb.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 source ./settings.conf
 
-echo "creating directory..."
-mkdir $SABNZB_CONFIG && sudo chmod a+rwx -R $SABNZB_CONFIG
+if [ ! "$(sudo docker ps -q -f name=sabnzbd)" ]; then
+    echo "Docker container already running...updating"
+    sudo docker stop sabnzbd
+    sudo docker rm sabnzbd
+    sudo docker pull linuxserver/sabnzbd
+else
+    echo "creating directory..."
+    mkdir $SABNZB_CONFIG && sudo chmod a+rwx -R $SABNZB_CONFIG
+fi
 
 echo "creating sabnzb..."
 # id -> gets PUID & PGID
@@ -18,10 +25,6 @@ sudo docker create \
   --restart unless-stopped \
   --net=host \
   linuxserver/sabnzbd
-
-# echo "init sabnzbd..."
-# sudo docker start sabnzbd
-# sudo docker stop sabnzbd
 
 # echo "setting up sabnzbd config..."
 # sudo cp -R $INIT/Sabnzb/* $SABNZB_CONFIG
